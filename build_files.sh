8c94affe2e65b41d27925eb91d80bfb130a7cf27
Anthony M <manthonysuresh@gmail.com>
2024-08-07 22:01:12 +0530

    updated

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,4 @@
-echo "BUILD START"
-python3.9 -m ensurepip --upgrade  # Ensure pip is installed
-python3.9 -m pip install --upgrade pip  # Upgrade pip to the latest version
-python3.9 -m pip install -r requirements.txt  # Install dependencies
-python3.9 manage.py collectstatic --noinput --clear  # Collect static files
-echo "BUILD END"
+#!/bin/bash
+pip install --upgrade pip
+pip install -r requirements.txt
+python manage.py collectstatic --noinput
