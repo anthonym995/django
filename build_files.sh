6a8daa338e6b44191cca53641f5275d0bfe9f358
Anthony M <manthonysuresh@gmail.com>
2024-08-07 22:57:37 +0530

    updated database

--- build_files.sh
+++ build_files.sh
@@ -2,6 +2,8 @@
 
 echo "BUILD START"
 # Ensure Python and pip are available, if necessary
+python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
+python3 manage.py migrate
 python3 manage.py collectstatic --noinput --clear
 echo "BUILD END"
