ab9188d09787201efa8ff881b4e5f7d17c6147c2
Anthony M <manthonysuresh@gmail.com>
2024-08-09 07:43:40 +0530

    updated requirement text file

--- firstdjango/settings.py
+++ firstdjango/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     '127.0.0.1',
     'localhost',
+    'node210a.containers.back4app.com',
     'node211a.containers.back4app.com',
     'djangoapp-ufw8qv2h.b4a.run',
 ]
